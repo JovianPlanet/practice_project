7c5240b504870a87b349342d542d1626ff995073
JovianPlanet <davidpunk@gmail.com>
2024-06-22 13:27:56 -0500

    Added requirements files and created venv

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -2,17 +2,22 @@
     to perform sentiment analysis
 """
 
-import requests
 import json
+import requests
 
 def sentiment_analyzer(text_to_analyse):
+    """
+    Analyzes sentimente in a text
+    returns dictionary wits sentiment label and score
+    """
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     injson = { "raw_document": { "text": text_to_analyse } }
 
-    response = requests.post(url, json=injson, headers=header)
+    response = requests.post(url, json=injson, headers=header)#, timeout=10)
     formatted_response = json.loads(response.text)
-
+    score = None
+    label = None
     if response.status_code == 200:
         label = formatted_response['documentSentiment']['label']
         score = formatted_response['documentSentiment']['score']
@@ -21,9 +26,3 @@ def sentiment_analyzer(text_to_analyse):
         score = None
 
     return {'label': label, 'score': score}
-
-'''
-URL: 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-Headers: {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-Input json: { "raw_document": { "text": text_to_analyse } }
-'''
